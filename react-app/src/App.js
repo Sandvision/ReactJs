6d9ca8e117aea8db58220545f3a09328b8d52784
Sandvision <san.251190@gmail.com>
2022-03-10 15:58:18 +0000

    intial-build

---
+++ react-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Article, Brand, Cta, Feature, Navbar } from './components'
+import {Blog, Footer, Header, Possibility, Features} from './containers'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
